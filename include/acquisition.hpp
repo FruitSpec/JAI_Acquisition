4002ec23f6608ab3550ebfbc8dc56a5676949254
MaayanShriki <99584728+MaayanShriki@users.noreply.github.com>
2023-10-02 11:13:44 +0300

    fix

--- include/acquisition.hpp
+++ include/acquisition.hpp
@@ -99,7 +99,8 @@ VideoConfig * parse_args(short fps, short exposure_rgb, short exposure_800, shor
                          const string& output_dir, bool output_clahe_fsi, bool output_equalize_hist_fsi,
                          bool output_rgb, bool output_800, bool output_975, bool output_svo, bool output_zed_gray,
                          bool output_zed_depth, bool output_zed_pc, bool view, bool transfer_data,
-                         bool pass_clahe_stream, bool debug_mode);
+                         bool pass_clahe_stream, bool debug_mode, const PvString* alc_true_areas,
+                         const PvString* alc_false_areas);
 
 void set_parameters_per_source(PvGenParameterArray *&lDeviceParams, const PvString& source, int auto_exposure_max,
                                const PvString &pixel_format);
